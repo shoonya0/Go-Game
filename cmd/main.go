270559210be474be62fab5ce9aad8ac01776d538
shoonya0 <117340271+shoonya0@users.noreply.github.com>
2025-12-08 18:09:30 +0530

    feat: added pixel Map for game level and map it with the game.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"player/internal/core"
@@ -19,6 +20,7 @@ type Game struct {
 	// ------ Entities ------
 	player *core.PlayerRuntime
 
+	LevelData       *ebiten.Image
 	Level           []core.Platform
 	DynamicQuadtree *core.DynamicQuadtree
 
@@ -28,11 +30,28 @@ type Game struct {
 	isDebug   bool
 }
 
+var doOnce = false
+
+func (g *Game) loadLevel() {
+	if !doOnce {
+		Level := core.LoadLevel(g.LevelData)
+		g.Level = Level
+		for i := range g.Level {
+			g.DynamicQuadtree.Insert(&g.Level[i])
+		}
+		fmt.Println("Level loaded")
+		doOnce = true
+	}
+}
+
 // var frame = 0
 
 // run 60 TPS
 // run automatically every frame
 func (g *Game) Update() error {
+
+	g.loadLevel()
+
 	// poll input -> call another function to handle input
 	system.HandleInput(&g.input)
 
@@ -61,16 +80,28 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 // run automatically every frame
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
+	// return 640, 480
+	return 1360, 768
 }
 
-// run Once
-func main() {
-
-	img, _, err := ebitenutil.NewImageFromFile("GideonGraves.png")
+func LoadImage(path string) *ebiten.Image {
+	img, _, err := ebitenutil.NewImageFromFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return img
+}
+
+const (
+	playerSpriteSheetPath = "../assets/GideonGraves.png"
+	screenWidth           = 1360
+	screenHeight          = 768
+)
+
+// run Once
+func main() {
+	img := LoadImage(playerSpriteSheetPath)
+	levelData := LoadImage(core.Level_1)
 
 	game := &Game{
 		state: core.ModeMenu,
@@ -82,25 +113,22 @@ func main() {
 			return &p
 		}(),
 		spriteSheet: img,
+		LevelData:   levelData,
 		score:       0,
 		tickCount:   0,
 		isDebug:     false,
-		Level: []core.Platform{
-			// Floor
-			{X: 0, Y: 400, Width: 640, Height: 80},
-			// Floating Platform
-			{X: 200, Y: 300, Width: 150, Height: 20},
-			{X: 400, Y: 200, Width: 100, Height: 20},
-		},
-		DynamicQuadtree: core.NewDynamicQuadtree(core.AABB{X: 0, Y: 0, Width: core.Level_1_Width, Height: core.Level_1_Height}),
+		Level:       []core.Platform{},
+		// DynamicQuadtree: nil,
+		DynamicQuadtree: core.NewDynamicQuadtree(core.AABB{X: 0, Y: 0, Width: float64(core.Level_1_Width), Height: float64(core.Level_1_Height)}),
 	}
 
 	// Add platforms to Quadtree
-	for i := range game.Level {
-		game.DynamicQuadtree.Insert(&game.Level[i])
-	}
+	// for i := range game.Level {
+	// 	game.DynamicQuadtree.Insert(&game.Level[i])
+	// }
 
-	ebiten.SetWindowSize(640, 480)
+	// ebiten.SetWindowSize(640, 480) // 640, 480
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Pirate Adventure")
 
 	if err := ebiten.RunGame(game); err != nil {
